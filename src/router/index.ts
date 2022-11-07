462e30898d48cf57f1e2bde4739bb6592667c482
Arnaud Lejeune <lejeune.arnaud@gmail.com>
2022-11-07 09:03:36 -0600

    automated layouts support

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
-import HomeView from '@/pages/HomeView.vue'
 
 const routes = setupLayouts(generatedRoutes)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
     ...routes,
   ],
 })
