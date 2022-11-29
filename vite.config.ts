fea490e18bff03bdba2d5fa7634881612b616a46
Arnaud Lejeune <lejeune.arnaud@gmail.com>
2022-11-29 17:26:26 -0600

    certificates ssl

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,16 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Markdown from 'vite-plugin-vue-markdown'
 import DefineOptions from 'unplugin-vue-define-options/vite'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('./.cert/localhost-key.pem'),
+      cert: fs.readFileSync('./.cert/localhost.pem'),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
